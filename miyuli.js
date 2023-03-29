3858696c4a9ee45ec9345dd858154b02218876e0
Nuril <49325887+l-Nuril-l@users.noreply.github.com>
2023-03-29 07:13:44 +0000

    stuff

--- miyuli.js
+++ miyuli.js
@@ -7,50 +7,50 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBit
 const { DISCORD_TOKEN: token, PREFIX: prefix } = process.env;
 
 const gpt = new ChatGPTAPI({
-    apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.OPENAI_API_KEY
 })
 
 client.once(Events.ClientReady, c => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
+  console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
 client.on(Events.MessageCreate, async message => {
-    console.log(message)
-    if (message.content.includes('https://discord.gg')) {
-        message.delete();
-        message.reply("Это бан...");
-    }
+  console.log(message)
+  if (message.content.includes('https://discord.gg')) {
+    message.delete();
+    message.reply("Это бан...");
+  }
 
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
 
-    if (command === 'бот') {
-        const attachment = new AttachmentBuilder('answer.wav');
-        message.channel.send({ files: [attachment] });
-    }
-    else if (command === 'help') {
-        const answer = '¯\_(ツ)_/¯';
-        message.channel.send(answer);
-    }
-    else if (command === 'ask') {
-        if (!(message.content.slice(5).length > 1)) return
-        const res = await gpt.sendMessage(message.content.slice(5), { onProgress: (e) => console.log(e) })
-        message.channel.send({ content: res.text });
-    }
-    else if (message.author.id === '602608266278731777') {
-        const attachment = new AttachmentBuilder('target-detected.mp3');
-        message.channel.send({ files: [attachment] });
-    }
-    else if (message.author.id === '288671468349292545') {
-        if (!message.guild) return;
-        const user = message.mentions.users.first();
-        const member = message.guild.member(user);
-        let str = '';
-        str = member.toString();
-        console.log(str);
-        message.channel.send(str);
-    }
+  if (command === 'бот') {
+    const attachment = new AttachmentBuilder('answer.wav');
+    message.channel.send({ files: [attachment] });
+  }
+  else if (command === 'help') {
+    const answer = '¯\_(ツ)_/¯';
+    message.channel.send(answer);
+  }
+  else if (command === 'ask') {
+    if (!(message.content.slice(5).length > 1)) return
+    const res = await gpt.sendMessage(message.content.slice(5))
+    message.channel.send({ content: res.text });
+  }
+  else if (message.author.id === '602608266278731777') {
+    const attachment = new AttachmentBuilder('target-detected.mp3');
+    message.channel.send({ files: [attachment] });
+  }
+  else if (message.author.id === '288671468349292545') {
+    if (!message.guild) return;
+    const user = message.mentions.users.first();
+    const member = message.guild.member(user);
+    let str = '';
+    str = member.toString();
+    console.log(str);
+    message.channel.send({ content: str });
+  }
 });
 
 
